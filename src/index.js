2baa8d0a8183c2c5ee308df6dc72de0a93aea0f2
AnastasiyaPozyomina <pozyomina@gmail.com>
2023-08-11 21:27:09 +0700

    Done reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
 module.exports = function reverse (n) {
-  
+    n > 0
+    ? (a = Number(("" + n).split("").reverse().join("")))
+    : (a = Number(("" + Math.abs(n)).split("").reverse().join("")));
+return a;
 }
